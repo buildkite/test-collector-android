06948a7138d30866b9a9ef92e2176d826821342a
Bhumil Soni <thebhumilsoni@gmail.com>
2022-06-06 22:23:57 +1000

    Migrate all build configurations from Groovy to Kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,8 +12,8 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-include ':example-project'
-include ':test-collector'
-include ':test-collector:instrumented-test-collector-library'
-include ':test-collector:unit-test-collector-plugin'
-include ':test-collector:test-data-uploader'
+include(":example-project")
+include(":test-collector")
+include(":test-collector:instrumented-test-collector-library")
+include(":test-collector:unit-test-collector-plugin")
+include(":test-collector:test-data-uploader")
