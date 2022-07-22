e7820ad9043012552b70703f8720ec6de819e759
Bhumil Soni <thebhumilsoni@gmail.com>
2022-07-22 19:15:52 +1000

    Create EnvironmentValues object to declare all environment variables

---
+++ test-collector/test-data-uploader/src/main/java/com/buildkite/test_data_uploader/util/EnvironmentValues.kt
@@ -0,0 +1,8 @@
+package com.buildkite.test_data_uploader.util
+
+object EnvironmentValues {
+
+    // Generic environment values
+    const val BUILDKITE_ANALYTICS_TOKEN = "BUILDKITE_ANALYTICS_TOKEN"
+    const val BUILDKITE_ANALYTICS_DEBUG_ENABLED = "BUILDKITE_ANALYTICS_DEBUG_ENABLED"
+}
