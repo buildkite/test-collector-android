71c8708bee36d426acda5b3c061294a955bcbc65
Bhumil Soni <thebhumilsoni@gmail.com>
2024-04-11 10:44:06 +1000

    Add instrumented test collector setup code documentation for example app

--- example/src/androidTest/kotlin/com/buildkite/test/collector/android/example/ExampleTestCollector.kt
+++ example/src/androidTest/kotlin/com/buildkite/test/collector/android/example/ExampleTestCollector.kt
@@ -2,6 +2,10 @@ package com.buildkite.test.collector.android.example
 
 import com.buildkite.test.collector.android.InstrumentedTestCollector
 
+/**
+ * Configures `ExampleTestCollector` with Buildkite test collector configurations.
+ * Values are derived from local/CI environment variables and set in [BuildConfig] at build time.
+ */
 class ExampleTestCollector : InstrumentedTestCollector(
     apiToken = BuildConfig.BUILDKITE_ANALYTICS_TOKEN,
     isDebugEnabled = BuildConfig.BUILDKITE_ANALYTICS_DEBUG_ENABLED
