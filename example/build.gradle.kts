a07bbc4452fb6407f7a6310016f6ca250892f9c0
Bhumil Soni <thebhumilsoni@gmail.com>
2024-06-07 13:39:13 +1000

    Access GitHub Actions environment variables in CI scripts

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,5 +1,4 @@
-import com.buildkite.test.collector.android.tracer.environment.TestEnvironmentValue.BUILDKITE_ANALYTICS_DEBUG_ENABLED
-import com.buildkite.test.collector.android.tracer.environment.TestEnvironmentValue.BUILDKITE_ANALYTICS_TOKEN
+import com.android.build.api.dsl.ApplicationDefaultConfig
 
 plugins {
     id("com.android.application")
@@ -22,11 +21,7 @@ android {
         testInstrumentationRunnerArguments["listener"] =
             "com.buildkite.test.collector.android.InstrumentedTestCollector"
 
-        // Passes environment variables as instrumentation arguments
-        testInstrumentationRunnerArguments[BUILDKITE_ANALYTICS_TOKEN] =
-            getEnv(BUILDKITE_ANALYTICS_TOKEN)
-        testInstrumentationRunnerArguments[BUILDKITE_ANALYTICS_DEBUG_ENABLED] =
-            getEnv(BUILDKITE_ANALYTICS_DEBUG_ENABLED)
+        setupTestCollectorEnvironment()
 
         vectorDrawables {
             useSupportLibrary = true
@@ -79,6 +74,32 @@ dependencies {
 
     androidTestImplementation(libs.androidx.test.ext)
     androidTestImplementation(libs.androidx.compose.ui.test)
+}
+
+fun ApplicationDefaultConfig.setupTestCollectorEnvironment() {
+    // Passes environment variables as instrumentation arguments
+    testInstrumentationRunnerArguments["BUILDKITE_ANALYTICS_TOKEN"] =
+        getEnv("BUILDKITE_ANALYTICS_TOKEN")
+    testInstrumentationRunnerArguments["BUILDKITE_ANALYTICS_DEBUG_ENABLED"] =
+        getEnv("BUILDKITE_ANALYTICS_DEBUG_ENABLED")
+    testInstrumentationRunnerArguments["GITHUB_ACTION"] =
+        getEnv("GITHUB_ACTION")
+    testInstrumentationRunnerArguments["GITHUB_RUN_ID"] =
+        getEnv("GITHUB_RUN_ID")
+    testInstrumentationRunnerArguments["GITHUB_RUN_NUMBER"] =
+        getEnv("GITHUB_RUN_NUMBER")
+    testInstrumentationRunnerArguments["GITHUB_RUN_ATTEMPT"] =
+        getEnv("GITHUB_RUN_ATTEMPT")
+    testInstrumentationRunnerArguments["GITHUB_REF_NAME"] =
+        getEnv("GITHUB_REF_NAME")
+    testInstrumentationRunnerArguments["GITHUB_REPOSITORY"] =
+        getEnv("GITHUB_REPOSITORY")
+    testInstrumentationRunnerArguments["GITHUB_SHA"] =
+        getEnv("GITHUB_SHA")
+    testInstrumentationRunnerArguments["GITHUB_WORKFLOW"] =
+        getEnv("GITHUB_WORKFLOW")
+    testInstrumentationRunnerArguments["GITHUB_ACTOR"] =
+        getEnv("GITHUB_ACTOR")
 }
 
 fun getEnv(key: String): String = System.getenv(key) ?: ""
